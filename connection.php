58a85735d0c886e8165a26c1f1498a02ba473ef8
muhammed erdem <erdem.muhammed1@outlook.com>
2022-01-25 21:50:41 +0100

    server name

--- connection.php
+++ connection.php
@@ -7,5 +7,9 @@ if (isset($_SERVER['HTTP_ORIGIN']))
     header('Access-Control-Max-Age: 86400');
 }
 
+$server_name = "localhost";
+$server_user = "root";
+$server_pass = "";
+$database_name = "project_final";
 
 ?>

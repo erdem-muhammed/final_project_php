d13090e0ecec1b19ad540f2d038681d6b8e1fe53
muhammed erdem <erdem.muhammed1@outlook.com>
2022-01-26 15:33:10 +0100

    empty up to date

--- register.php
+++ register.php
@@ -1,4 +1,12 @@
 <?php
 include("connection.php");
 
+
+
+if(empty($_POST["name"]) || empty($_POST["surname"]) ||
+empty($_POST["email"]) || empty($_POST["password"] ) || 
+empty($_POST["phone"]))
+{
+    return;
+}
 ?>
